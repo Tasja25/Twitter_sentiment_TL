cd2089f6cd51b7ffbaad2b2d8f422563b08ad71c
Tasja25 <99636512+Tasja25@users.noreply.github.com>
2022-10-02 11:55:47 +0200

    First commit

---
+++ StreamlitTest.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.write("""
+# Twitter Sentiment Classification
+## Introduction
+Eskom wants to evaluate likelihood of success if they decide to invest in renewable energy.
+We were provided with historical twitter feed data, together with climate change sentiment categories, based on tweets.
+Eskom has also provided a sample of population tweets so that we can assist in determining climate change beliefs, based on the sample provided by Eskom.
+This will inform Eskom as to how much marketing & education needs to happen in the public space, in order for renewable energy investment to be more successful.""")
